27b3680e39048824a7ec1500abc2393b6ce32113
root <root@LAPTOP-9EI67U4D.localdomain>
2020-12-28 11:25:29 +0900

    #3 function FoodList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import Proptypes from 'prop-types'
 
-function Food({food}){
-  return <h1>I like {food} !!</h1>
+function Food({name,url}){
+  return (
+    <>
+      <h2>I like {name} !!</h2>
+      <a href={url}>Here is Location</a>
+    </>
+  )
+}
+
+const FoodList = [
+  {
+    id:1,
+    name:"Hamburger",
+    URL: "www.naver.com",
+    rating: 5
+  },
+  {
+    id:2,
+    name:"Sandwich",
+    URL: "www.naver.com",
+    rating: 4.9
+  },
+  {
+    id:3,
+    name:"Pizza",
+    URL: "www.naver.com",
+    rating: 3
+  },
+  {
+    id:4,
+    name:"Curry",
+    URL: "www.naver.com",
+    rating: 2.1
+  }
+]
+
+function renderFood(dish){
+  console.log(dish)
+  return <Food key={dish.id} name={dish.name} url={dish.URL}/>
 }
 
 function App() {
-  return (<Food food="Hamburger"/>
+  return (
+    <div>
+      {FoodList.map(renderFood)}
+    </div>
   );
 }
 
