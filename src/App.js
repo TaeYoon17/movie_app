f1a7e58dcfadd0f6b88534ee3b0252c9a5f3accc
root <root@LAPTOP-9EI67U4D.localdomain>
2020-12-29 11:53:59 +0900

    #5 setState and mount

--- src/App.js
+++ src/App.js
@@ -1,60 +1,37 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function Food({name,url,rating}){
-  return (
-    <>
-      <h2>I like {name} !!</h2>
-      <a href={url}>Here is Location</a>
-      <span>{rating}/5</span>
-    </>
-  )
-}
-
-cosnt Food.PropTypes={
-  name: PropTypes.string.isRequired,
-  url: PropTypes.string.isRequired,
-  rating:PropTypes.number.isRequired
-}
-
-const FoodList = [
-  {
-    id:1,
-    name:"Hamburger",
-    URL: "www.naver.com",
-    rating: 5
-  },
-  {
-    id:2,
-    name:"Sandwich",
-    URL: "www.naver.com",
-    rating: 4.9
-  },
-  {
-    id:3,
-    name:"Pizza",
-    URL: "www.naver.com",
-    rating: 3
-  },
-  {
-    id:4,
-    name:"Curry",
-    URL: "www.naver.com",
-    rating: 2.1
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    console.log("hello");
   }
-]
-
-function renderFood(dish){
-  console.log(dish)
-  return <Food key={dish.id} name={dish.name} url={dish.URL} rating={dish.rating}/>
-}
-
-function App() {
-  return (
-    <div>
-      {FoodList.map(renderFood)}
-    </div>
-  );
+  state={
+    cnt:0
+  };
+  plus= () =>{
+    this.setState(current=>({cnt:current.cnt+1}));
+  };
+  minus= () =>{
+    this.setState(current=>({cnt:current.cnt-1}));
+  };
+  componentDidMount(){
+    console.log("hello@-@");
+  };
+  componentDidUpdate(){
+    console.log("I just Updated!!");
+  }
+  componentWillUnmount(){
+    console.log("I just amounted");
+  }
+  render(){
+    return (
+      <>
+        <h1>This number is: {this.state.cnt}</h1>
+        <button onClick={this.plus}>plus</button>
+        <button onClick={this.minus}>minus</button>
+      </>
+      )
+  };
 }
 
 export default App;
